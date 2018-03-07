62374e525357f1be6acc1fb4420dddca20441038
Tatsuro Moriyama <t4traw@gmail.com>
2018-03-07 12:51:45 +0900

    CreateJSで戦った歴史

--- components/foobar.vue
+++ components/foobar.vue
@@ -1,25 +1,35 @@
 <template lang="ejs">
-  <div>
-    <h2>{{ msg }}</h2>
-    <input v-model="msg">
+  <div class="block">
+    <div class="">
+      <canvas ref="canvas" width="800" height="200"></canvas>
+    </div>
+    <div class="">
+      <input v-model="foobar">
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+  head: {
+    script: [
+      { src: 'https://code.createjs.com/1.0.0/easeljs.min.js' }
+    ],
+  },
   data() {
     return {
-      msg: "FooBar"
+      foobar: 'FooBar'
     }
   },
+  // mounted: function(){
+  //   stage.update()
+  // }
 }
 </script>
 
 <style lang="sass" scoped>
-div
-  height: 100px
-  background-color: blue
+.block
+  background-color: rgb(218, 214, 156)
   margin-bottom: 1em
-  h2
-    color: #ffffff
 </style>
